437729867944ebdbd8581509d3e92bea40b11a16
FaresEsprit20 <63284587+FaresEsprit20@users.noreply.github.com>
2021-09-08 13:54:38 +0100

    second commit

--- index.php
+++ index.php
@@ -125,22 +125,99 @@
 
       
       <div class="products-table mt-5 mb-5">
-        <div class="table-responsive">
+        <div class="table-reservations">
 
-          <table class="table">
-            <caption>Liste de production</caption>
-            <thead class="table-dark">
-              <th scope="col">#</th>
-              <th scope="col">Ligne</th>
-              <th scope="col">Produit</th>
-              <th scope="col">Date</th>
-              <th scope="col">Heure</th>
-              <th scope="col">Qte</th>
-            </thead>
-            <tbody id="tbodydaily">
-              
-            </tbody>
-          </table>
+  
+
+        <form class="row g-3">
+        <div class="col-md-9">
+    <label for="inputState" class="form-label">State</label>
+    <select id="inputState" class="form-select">
+      <option selected>Choose...</option>
+      <option>...</option>
+    </select>
+  </div>
+  <div class="col-md-4">
+    <label for="inputState" class="form-label">State</label>
+    <select id="inputState" class="form-select">
+      <option selected>Choose...</option>
+      <option>...</option>
+    </select>
+  </div>
+  <div class="col-md-4">
+    <label for="inputState" class="form-label">State</label>
+    <select id="inputState" class="form-select">
+      <option selected>Choose...</option>
+      <option>...</option>
+    </select>
+  </div>
+  <div class="col-md-4">
+    <label for="inputState" class="form-label">State</label>
+    <select id="inputState" class="form-select">
+      <option selected>Choose...</option>
+      <option>...</option>
+    </select>
+  </div>
+  <div class="col-md-6">
+    <label for="inputState" class="form-label">State</label>
+    <select id="inputState" class="form-select">
+      <option selected>Choose...</option>
+      <option>...</option>
+    </select>
+  </div>
+  <div class="col-md-6">
+    <label for="inputState" class="form-label">State</label>
+    <select id="inputState" class="form-select">
+      <option selected>Choose...</option>
+      <option>...</option>
+    </select>
+  </div>
+  <div class="col-md-6">
+    <label for="inputEmail4" class="form-label">Email</label>
+    <input type="email" class="form-control" id="inputEmail4">
+  </div>
+  <div class="col-md-6">
+    <label for="inputPassword4" class="form-label">Password</label>
+    <input type="password" class="form-control" id="inputPassword4">
+  </div>
+  <div class="col-12">
+    <label for="inputAddress" class="form-label">Address</label>
+    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+  </div>
+  <div class="col-12">
+    <label for="inputAddress2" class="form-label">Address 2</label>
+    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+  </div>
+  <div class="col-md-6">
+    <label for="inputCity" class="form-label">City</label>
+    <input type="text" class="form-control" id="inputCity">
+  </div>
+  <div class="col-md-4">
+    <label for="inputState" class="form-label">State</label>
+    <select id="inputState" class="form-select">
+      <option selected>Choose...</option>
+      <option>...</option>
+    </select>
+  </div>
+  <div class="col-md-2">
+    <label for="inputZip" class="form-label">Zip</label>
+    <input type="text" class="form-control" id="inputZip">
+  </div>
+  <div class="col-12">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="gridCheck">
+      <label class="form-check-label" for="gridCheck">
+        Check me out
+      </label>
+    </div>
+  </div>
+  <div class="col-12">
+    <button type="submit" class="btn btn-primary">Sign in</button>
+  </div>
+
+</form>
+
+
       
         </div>
 
--- models/EleveModel.php
+++ models/EleveModel.php
@@ -20,8 +20,90 @@ class EleveModel {
        $this->conn = $db;
     }
     
+    public function getAllElevesByGroup() {
+        $json = file_get_contents('php://input');
+        $data = json_decode($json, true);
+      
+        $stmt = $this->conn->prepare("SELECT * FROM  groupe_eleve GROUP BY id_groupe");
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($result) ;
+      
+    }
 
-   
+    public function AddEleve() {
+        $json = file_get_contents('php://input');
+        $data = json_decode($json, true);
+        if( isset($data["prenom_eleve"]) and isset($data["nom_eleve"]) and isset($data["classe"]) and isset($data["num_tel"]) ){
+        $stmt = $this->conn->prepare("INSERT INTO eleves VALUES(?,?,?,?,?) ");
+        $stmt->execute([0,$data["prenom_eleve"],$data["nom_eleve"],$data["classe"],$data["num_tel"]]);
+        echo json_encode(http_response_code(201));
+    }else {
+        http_response_code(401);
+        die();
+    }
+      
+    }
+ 
+
+    public function UpdateEleve() {
+        $json = file_get_contents('php://input');
+        $data = json_decode($json, true);
+        if(isset($data["prenom_eleve"]) and isset($data["nom_eleve"]) and isset($data["classe"]) and isset($data["num_tel"]) ){
+        $stmt = $this->conn->prepare(" UPDATE eleves SET prenom_eleve=?,nom_eleve=?,classe=?,num-tel=? ");
+        $stmt->execute([$data["prenom_eleve"],$data["nom_eleve"],$data["classe"],$data["num_tel"]]);
+        echo json_encode(http_response_code(201));
+    }else {
+        http_response_code(401);
+        die();
+    }
+      
+    }
+    
+    public function DeleteEleveById() {
+        $json = file_get_contents('php://input');
+        $data = json_decode($json, true);
+        if(isset($data["code_eleve"]) ){
+        $stmt = $this->conn->prepare("DELETE FROM eleves WHERE code_eleve = ? ");
+        $stmt->execute([$data["code_eleve"]]);
+        echo json_encode(http_response_code(201));
+    }else {
+        http_response_code(401);
+        die();
+    }
+      
+    }
+
+
+    public function GetEleveById() {
+        $json = file_get_contents('php://input');
+        $data = json_decode($json, true);
+        if(isset($data["code_eleve"]) ){
+        $stmt = $this->conn->prepare("SELECT * FROM eleves WHERE code_eleve = ? ");
+        $stmt->execute([$data["code_eleve"]]);
+        echo json_encode(http_response_code(201));
+    }else {
+        http_response_code(401);
+        die();
+    }
+      
+    }
+
+    public function GetAllEleves() {
+        $json = file_get_contents('php://input');
+        $data = json_decode($json, true);
+        if(isset($data["code_eleve"]) ){
+        $stmt = $this->conn->prepare("SELECT * FROM eleves ");
+        $stmt->execute([$data["code_eleve"]]);
+        echo json_encode(http_response_code(201));
+    }else {
+        http_response_code(401);
+        die();
+    }
+      
+    }
+
+
 
 
 }
