df92f9120ea8243c8c0169da028c3984b1639ba3
FaresEsprit20 <63284587+FaresEsprit20@users.noreply.github.com>
2021-09-09 14:46:19 +0100

    second commit

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
         <div class="container-fluid">
           <a class="navbar-brand" href="#">
             <img src="assets/images/logo/logo.jpeg" alt="" width="30" height="24" class="d-inline-block align-text-top">
-            PWA
+            Reserv App
           </a>
           <!--bouton fermée de toggle  btn-close -->
           <button class="navbar-toggler shadow-none" id="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
@@ -40,8 +40,12 @@
                 <i class="navbar-fa fa fa-pencil-square fa-2x d-inline-block"></i>
               </li>
               <li class="nav-item">
-                <a class="nav-link d-inline-block" href="#products">Réservations</a>
+                <a class="nav-link d-inline-block" href="#reservations">Réservations</a>
                 <i class="navbar-fa fa fa-product-hunt fa-2x d-inline-block"></i>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link d-inline-block" href="#groupes">Groupes</a>
+                <i class="navbar-fa fa fa-users fa-2x d-inline-block"></i>
               </li>
               <li class="nav-item">
                 <a class="nav-link d-inline-block" href="#stats">Statistiques</a>
@@ -124,19 +128,19 @@
     <div class="col-12 col-sm-12 col-lg-8">
 
       
-      <div class="products-table mt-5 mb-5">
+      <div class="products-table mt-5 mb-5 pt-5 pb-5">
         <div class="table-reservations">
 
   
 
         <form class="row g-3">
-        <div class="col-md-9">
+        <div class="col-md-10">
     <label for="inputSalle" class="form-label">Choisir Salle</label>
     <select id="inputSalle" class="form-select">
       <option selected disabled>Salle...</option>
     </select>
   </div>
-  <div class="col-md-4">
+  <div class="col-md-3">
     <label for="inputHeure" class="form-label">Début de réservation</label>
     <select id="inputHeure" class="form-select">
       <option selected disabled>Heure...</option>
@@ -166,7 +170,7 @@
           <option value="23">23</option>
     </select>
   </div>
-  <div class="col-md-4">
+  <div class="col-md-3">
   <label for="inputJour" style="visibility:hidden" class="form-label">Début de réservation</label>
     <select id="inputJour" class="form-select">
       <option selected disabled>Jour...</option>
@@ -202,7 +206,7 @@
           <option value="31">31</option>
     </select>
   </div>
-  <div class="col-md-4">
+  <div class="col-md-3">
   <label for="inputMois" style="visibility:hidden" class="form-label">Début de réservation</label>
     <select id="inputMois" class="form-select">
       <option selected disabled>Mois...</option>
@@ -220,21 +224,130 @@
           <option value="12">Décembre</option>
     </select>
   </div>
+  <div class="col-md-3">
+  <label for="inputAndeb" style="visibility:hidden" class="form-label">Début de réservation</label>
+    <select id="inputAndeb" class="form-select">
+      <option selected disabled>An début...</option>
+      <option value="2021">2021</option>
+      <option value="2022">2022</option>
+      <option value="2023">2023</option>
+      <option value="2024">2024</option>  
+    </select>
+  </div>
+
+  <!-- Partie Fin de Réservation -->
+  <div class="col-md-3">
+    <label for="inputHeureFin" class="form-label">Fin de réservation</label>
+    <select id="inputHeureFin" class="form-select">
+      <option selected disabled>Heure...</option>
+          <option value="00">00</option>
+          <option value="01">01</option>
+          <option value="02">02</option>
+          <option value="03">03</option>
+          <option value="04">04</option>
+          <option value="05">05</option>
+          <option value="06">06</option>
+          <option value="07">07</option>
+          <option value="08">08</option>
+          <option value="09">09</option>
+          <option value="10">10</option>
+          <option value="11">11</option>
+          <option value="12">12</option>
+          <option value="13">13</option>
+          <option value="14">14</option>
+          <option value="15">15</option>
+          <option value="16">16</option>
+          <option value="17">17</option>
+          <option value="18">18</option>
+          <option value="19">19</option>
+          <option value="20">20</option>
+          <option value="21">21</option>
+          <option value="22">22</option>
+          <option value="23">23</option>
+    </select>
+  </div>
+  <div class="col-md-3">
+  <label for="inputJourFin" style="visibility:hidden" class="form-label">Fin de réservation</label>
+    <select id="inputJourFin" class="form-select">
+      <option selected disabled>Jour...</option>
+          <option value="01">1</option>
+          <option value="02">2</option>
+          <option value="03">3</option>
+          <option value="04">4</option>
+          <option value="05">5</option>
+          <option value="06">6</option>
+          <option value="07">7</option>
+          <option value="08">9</option>
+          <option value="10">10</option>
+          <option value="11">11</option>
+          <option value="12">12</option>
+          <option value="13">13</option>
+          <option value="14">14</option>
+          <option value="15">15</option>
+          <option value="16">16</option>
+          <option value="17">17</option>
+          <option value="18">18</option>
+          <option value="19">19</option>
+          <option value="20">20</option>
+          <option value="21">21</option>
+          <option value="22">22</option>
+          <option value="23">23</option>
+          <option value="24">24</option>
+          <option value="25">25</option>
+          <option value="26">26</option>
+          <option value="27">27</option>
+          <option value="28">28</option>
+          <option value="29">29</option>
+          <option value="30">30</option>
+          <option value="31">31</option>
+    </select>
+  </div>
+  <div class="col-md-3">
+  <label for="inputMoisFin" style="visibility:hidden" class="form-label">Fin de réservation</label>
+    <select id="inputMoisFin" class="form-select">
+      <option selected disabled>Mois...</option>
+          <option value="01">Janvier</option>
+          <option value="02">Février</option>
+          <option value="03">Mars</option>
+          <option value="04">Avril</option>
+          <option value="05">Mai</option>
+          <option value="06">Juin</option>
+          <option value="07">Juillet</option>
+          <option value="08">Aout</option>
+          <option value="09">Séptembre</option>
+          <option value="10">Octobre</option>
+          <option value="11">Novembre</option>
+          <option value="12">Décembre</option>
+    </select>
+  </div>
+  <div class="col-md-3">
+  <label for="inputAnFin" style="visibility:hidden" class="form-label">Fin de réservation</label>
+    <select id="inputAnFin" class="form-select">
+      <option selected disabled>An début...</option>
+      <option value="2021">2021</option>
+      <option value="2022">2022</option>
+      <option value="2023">2023</option>
+      <option value="2024">2024</option>  
+    </select>
+  </div>
+
+<!-- Fin Partie Fin de Réservation -->
 
   <div class="col-md-6">
     <label for="reserv_cin" class="form-label">CIN</label>
     <input type="text" class="form-control" id="reserv_cin">
   </div>
   <div class="col-md-6">
-    <label for="reserv_groupe" class="form-label">Nom Groupe</label>
-    <input type="text" class="form-control" id="reserv_groupe">
+  <label for="inputreservgroup" class="form-label">Groupe</label>
+    <select id="inputAreservgroup" class="form-select">
+      <option selected disabled>Sélectionner un groupe...</option>
+    </select>
   </div>
-  
   
   <div class="col-12">
     <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck">
-      <label class="form-check-label" for="gridCheck">
+      <input class="form-check-input" type="checkbox" id="reservCheck" required>
+      <label class="form-check-label" for="reservCheck">
         Cochez moi
       </label>
     </div>
@@ -260,7 +373,57 @@
 <!-- End Reservation-->
 
 
+<!-- Start Groupes -->
 
+<section class="groupes" id="groupes">
+  <div class="container-fluid">
+    <div class="special-heading">Groupes</div>
+      <p>Créer un groupe</p>
+  <div class="row">
+  <div class="col col-sm col-lg-2">
+  </div>
+    <div class="col-12 col-sm-12 col-lg-8">
+
+      
+      <div class="products-table mt-5 mb-5">
+        <div class="table-reservations">
+
+  
+
+        <form class="row g-3">
+       
+
+  <div class="col-md-8">
+    <label for="nom_groupe" class="form-label">Nom Groupe</label>
+    <input type="text" class="form-control" id="nom_groupe">
+  </div>
+  <div class="col-12">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="reservCheck" required>
+      <label class="form-check-label" for="reservCheck">
+        Cochez moi
+      </label>
+    </div>
+  </div>
+  <div class="col-12">
+    <button type="submit" class="btn btn-dark">Confirmer Réservation</button>
+  </div>
+
+</form>
+
+
+      
+        </div>
+
+    </div>
+  </div>
+  <div class="col col-sm col-lg-2">
+  </div>
+  </div>
+ 
+</section>
+
+<!-- End Reservation-->
 
 
 <!-- Start Products -->
