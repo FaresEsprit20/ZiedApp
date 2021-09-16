eec6a46d1fdecb5a14a9b20e7e5dd3993c3946c2
FaresEsprit20 <63284587+FaresEsprit20@users.noreply.github.com>
2021-09-16 21:33:14 +0100

    sixth commit

--- models/EleveModel.php
+++ models/EleveModel.php
@@ -58,12 +58,28 @@ class EleveModel {
     }
  
 
-    public function UpdateEleve() {
+    public function updateEleve() {
         $json = file_get_contents('php://input');
         $data = json_decode($json, true);
-        if(isset($data["prenom_eleve"]) and isset($data["nom_eleve"]) and isset($data["classe"]) and isset($data["num_tel"]) ){
-        $stmt = $this->conn->prepare(" UPDATE eleves SET prenom_eleve=?,nom_eleve=?,classe=?,num_tel=? ");
+        $selected = array();
+       
+        if(isset($data["selectedGroups"])){
+            foreach($data["selectedGroups"] as $item){
+            array_push($selected,$item);
+            }
+        }
+     
+        if( isset($data["prenom_eleve"]) and isset($data["nom_eleve"]) and isset($data["classe"]) and isset($data["num_tel"]) ){
+        $stmt = $this->conn->prepare("UPDATE eleves SET prenom_eleve=?,nom_eleve=?,classe=?,num_tel=?");
         $stmt->execute([$data["prenom_eleve"],$data["nom_eleve"],$data["classe"],$data["num_tel"]]);
+        $lastinsertedId = $this->conn->lastInsertId();
+        foreach($selected as $item){
+        $stmt2 = $this->conn->prepare("DELETE from groupe_eleve WHERE id_eleve = ? ");
+          $stmt2->execute([$lastinsertedId]);
+          $stmt3 = $this->conn->prepare("INSERT INTO groupe_eleve VALUES(?,?) ");
+          $stmt3->execute([$item,$lastinsertedId]);
+    }
+
         echo json_encode(http_response_code(201));
     }else {
         http_response_code(401);
@@ -71,6 +87,7 @@ class EleveModel {
     }
       
     }
+
     
     public function DeleteEleveById() {
         $json = file_get_contents('php://input');
@@ -102,16 +119,12 @@ class EleveModel {
     }
 
     public function GetAllEleves() {
-        $json = file_get_contents('php://input');
-        $data = json_decode($json, true);
-        if(isset($data["code_eleve"]) ){
+        
+      
         $stmt = $this->conn->prepare("SELECT * FROM eleves ");
-        $stmt->execute([$data["code_eleve"]]);
-        echo json_encode(http_response_code(201));
-    }else {
-        http_response_code(401);
-        die();
-    }
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($result);
       
     }
 
