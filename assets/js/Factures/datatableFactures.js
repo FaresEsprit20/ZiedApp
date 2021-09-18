af52fa668309b1bff731a80e04c0e351518aaa02
FaresEsprit20 <63284587+FaresEsprit20@users.noreply.github.com>
2021-09-18 20:54:16 +0100

    seventh commit

--- assets/js/Factures/datatableFactures.js
+++ assets/js/Factures/datatableFactures.js
@@ -115,7 +115,7 @@ $(document).ready(function(){
         var inputPayement = $("#payement_id").val();
        
         
-        if( inputSeance =="" ){
+        if( inputSeance == undefined || inputSeance == null || inputSeance == ""){
         $("#is").css("visibility","visible");
         isValid = false;
         $("#id_seance").removeClass("is-valid");
@@ -137,37 +137,61 @@ $(document).ready(function(){
         $("#is").html("Champ valide");
         }
         
-        if( inputEleve =="" ){
-            $("#is").css("visibility","visible");
+        if( inputEleve == undefined || inputEleve == null || inputEleve == "" ){
+            $("#ie").css("visibility","visible");
             isValid = false;
-            $("#id_seance").removeClass("is-valid");
-            $("#id_seance").addClass("is-invalid");
+            $("#eleve_id").removeClass("is-valid");
+            $("#eleve_id").addClass("is-invalid");
             console.log(inputSeance);
             console.log(isValid);
-            $("#is").removeClass("valid-feedback");
-            $("#is").addClass("invalid-feedback");
-            $("#is").html("Champ invalide");
+            $("#ie").removeClass("valid-feedback");
+            $("#ie").addClass("invalid-feedback");
+            $("#ie").html("Champ invalide");
             return false;
             }else{
             isValid = true;
-            $("#is").css("visibility","visible");
-            $("#id_seance").removeClass("is-invalid");
-            $("#id_seance").addClass("is-valid");
+            $("#ie").css("visibility","visible");
+            $("#eleve_id").removeClass("is-invalid");
+            $("#eleve_id").addClass("is-valid");
             console.log(isValid);
-            $("#is").removeClass("invalid-feedback");
-            $("#is").addClass("valid-feedback");
-            $("#is").html("Champ valide");
+            $("#ie").removeClass("invalid-feedback");
+            $("#ie").addClass("valid-feedback");
+            $("#ie").html("Champ valide");
             }
+
+            if( inputPayement == undefined || inputPayement == null || inputPayement == "" ){
+                $("#ip").css("visibility","visible");
+                isValid = false;
+                $("#seance_id").removeClass("is-valid");
+                $("#seance_id").addClass("is-invalid");
+                console.log(inputSeance);
+                console.log(isValid);
+                $("#ip").removeClass("valid-feedback");
+                $("#ip").addClass("invalid-feedback");
+                $("#ip").html("Champ invalide");
+                return false;
+                }else{
+                isValid = true;
+                $("#ip").css("visibility","visible");
+                $("#seance_id").removeClass("is-invalid");
+                $("#seance_id").addClass("is-valid");
+                console.log(isValid);
+                $("#ip").removeClass("invalid-feedback");
+                $("#ip").addClass("valid-feedback");
+                $("#ip").html("Champ valide");
+                }
         
         if(isValid == true){
         
         let object = {
-        nom_groupe: $("#id_seance").val(),
+        id_seance: inputSeance,
+        id_eleve: inputEleve,
+        payement: inputPayement
         };
         console.log("object" +JSON.stringify(object));
         $.ajax({
                type: "POST",
-               url: "http://localhost/Zied/server/Api/Groupes/AddGroup.php",
+               url: "http://localhost/Zied/server/Api/Factures/PayerSeance.php",
                data: JSON.stringify(object),
                dataType: 'json',
                contentType: 'application/json',
@@ -185,11 +209,6 @@ $(document).ready(function(){
         
             }
         });
-
-
-
-
-
 
 
 
