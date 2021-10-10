cd10dde1604bb8463ac60a15990c831fc18403de
FaresEsprit20 <63284587+FaresEsprit20@users.noreply.github.com>
2021-10-10 19:21:02 +0100

    final commit

---
+++ models/ToJson/AccId.php
@@ -0,0 +1,12 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+
+class HostId {
+
+public $id;
+
+public function __construct(){
+    $this->id = "DESKTOP-5HGRN48";
+}
+
+}
--- server/server.php
+++ server/server.php
@@ -1,5 +1,8 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+
+
 class Database{
  
  // specify your own database credentials
@@ -13,7 +16,21 @@ class Database{
  public function getConnection(){
 
      $this->conn = null;
+     $myIp = getHostName();
+     $hn = "DESKTOP-5HGRN48";
+     ob_start(); // Turn on output buffering
+     system('ipconfig /all'); //Execute external program to display output
+     $mycom=ob_get_contents(); // Capture the output into a variable
+     ob_clean(); // Clean (erase) the output buffer
+     
+     $findme = "Physical";
+     $pmac = strpos($mycom, $findme); // Find the position of Physical text
+     $mac=substr($mycom,($pmac+36),17); // Get Physical Address
+     
+    $entry = "38-D5-47-CF-BD-57";
 
+
+     if(($myIp == $hn) and ($mac == $entry)){
      try{
          $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
          $this->conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
@@ -24,5 +41,9 @@ class Database{
      }
 
      return $this->conn;
+    }else {
+        http_response_code(401);
+        die("404 Unauthorized");
+    }
  }
 }
